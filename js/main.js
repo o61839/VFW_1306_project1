92dc15bd00654f915c681119cd019af5a9952fe1
Jenney Grover <jenneygrover@fullsail.edu>
2013-06-10 23:45:05 -0400

    added html and js
    added id to select option
    added js variables

--- js/main.js
+++ js/main.js
@@ -4,3 +4,49 @@
 	The Satchel
 	June 13, 2013
 */
+
+//JavaScript Methods: .getElementByID()
+
+var nameBook = document.getelementByID("bname");
+var nameAuthor = document.getelementByID("aname");
+var isbnNumber = document.getelementByID("isbn");
+var readBook = document.getelementByID("read");
+var readBookNot = document.getelementByID("not_read");
+var dateAdded = document.getelementByID("dateAdded");
+var datePublished = document.getelementByID("datePublished");
+var purchased = document.getelementByID("purchased");
+var borrowed = document.getelementByID("borrowed");
+var paperback = document.getelementByID("paperback");
+var hardcover = document.getelementByID("hardcover");
+var mobile = document.getelementByID("mobile");
+var fiction = document.getelementByID("fiction");
+var nonFiction = document.getelementByID("nonfiction");
+var submitButton = document.getelementByID("submitButton");
+var poetry = document.getelementByID("poetry");
+var paranormal = document.getelementByID("paranormal");
+var romance = document.getelementByID("romance");
+var classics = document.getelementByID("classics");
+var comicGraphic = document.getelementByID("comic_graphic");
+var scifi = document.getelementByID("scifi");
+var crime = document.getelementByID("crime");
+var fantasy = document.getelementByID("fantasy");
+var horror = document.getelementByID("horror");
+var witchesWizardsMagic = document.getelementByID("witches_wizards_magic");
+var autobiography = document.getelementByID("autobiography");
+var design = document.getelementByID("design");
+var houseGarden = document.getelementByID("house_and_garden");
+var music = document.getelementByID("music");
+var biography = document.getelementByID("biography");
+var businessFinance = document.getelementByID("business_finance");
+var languages = document.getelementByID("languages");
+var sports = document.getelementByID("sports");
+var recipes = document.getelementByID("recipes");
+var travelGuides = document.getelementByID("travel_guides");
+var trueCrime = document.getelementByID("true_crime");
+var healthMindBody = document.getelementByID("health_mind_body");
+var history = document.getelementByID("history");
+var religion = document.getelementByID("religion");
+var computers = document.getelementByID("computers");
+var technical = document.getelementByID("technical");
+var craftHobby = document.getelementByID("craft_hobby");
+var exercise = document.getelementByID("exercise");
