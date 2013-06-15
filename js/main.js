98c6917660ef43bf924c70fa688a0c771e78a262
Jenney Grover <jenneygrover@fullsail.edu>
2013-06-15 17:24:14 -0400

    video 6
    video 6 watched and code updated…
    some code shows on form. some does not. hmmm?

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ window.addEventListener("DOMContentLoaded", function(){
 	function $(x){
 		var theElement = document.getElementById(x);
 		return theElement; 
-	}
+	};
 	
 	//Create select field element and populate with the options
 	function genreCategory (){
@@ -27,7 +27,7 @@ window.addEventListener("DOMContentLoaded", function(){
 			makeSelect.appendChild(makeOption);
 		}
 		selectLi.appendChild(makeSelect); 
-	}
+	};
 	
 	//functions to read the radio buttons
 	//could use the - var selected = document.forms[0].choice; - as another options instead of getElementById("myBookQuestions").choice;
@@ -75,15 +75,20 @@ window.addEventListener("DOMContentLoaded", function(){
 		}
 	};
 	
+	//function to reset the form after saving. 
+	function resetForm(){
+			document.getElementById("myBookQuestions").reset();
+   	};
+	
 	//function to save the form data into local storage. 
 	function submitInfo(){
 		keyValue 			= Math.floor(Math.random()*100001);
 		//Gather up all our form field values and store in an object. 
 		//Object properties contain array with the form label and input value. 
-		readBookChoices();
-		permanentBookChoices();
-		coverBookChoices();
-		typeBookChoices();
+		var readIT = readBookChoices();
+		var boughtIT = permanentBookChoices();
+		var hsmCover = coverBookChoices();
+		var trueFalse = typeBookChoices();
 		var myData			= {}; 
 			myData.nameBook 	= ["Name", $("bname").value];
 			myData.nameAuthor 	= ["Author", $("aname").value];
@@ -92,16 +97,46 @@ window.addEventListener("DOMContentLoaded", function(){
 			myData.datePublish 	= ["Published", $("datePublished").value];
 			myData.comments 	= ["Comments", $("comments").value];
 			myData.rating		= ["Rate", $("rating").value]; 
-			myData.genre		= ["Genre", $("selectGenre").value];
-			myData.readBook		= ["Read", readBookChoices];
-			myData.purchased	= ["Purchased", permanentBookChoices];
-			myData.cover		= ["Cover", coverBookChoices];
-			myData.fiction		= ["Type", typeBookChoices];
-		//Save data into Local Storage: use Stringify to convert our object to a string.
+			myData.genre		= ["Genre", $("selectGenre")];
+			myData.readBook		= ["Read", readIT];
+			myData.purchased	= ["Purchased", boughtIT];
+			myData.cover		= ["Cover", hsmCover];
+			myData.fiction		= ["Type", trueFalse];
+		//Save data into Local Storage: use Stringify to convert our object to a string by using JSON.stringify
 		localStorage.setItem(keyValue, JSON.stringify(myData)); 
 		alert("Your book is in your Satchel");
 		resetForm(); 
-	}
+	};
+	
+	//Write data from Local Storage to Browser
+	function showInfo(){
+		if(localStorage.length === 0){
+				alert("Your Satchel is empty.");
+		}
+		var makeDiv 	= $("bookInfoDisplay"); 
+		var makeList 	= document.createElement("ul");
+		makeDiv.appendChild(makeList); 
+		document.body.appendChild(makeDiv); 
+		for(var i=0, j=localStorage.length; i<j; i++){
+			var makeli 	= document.createElement("li");
+			makeList.appendChild(makeli); 
+			var key 	= localStorage.key(i); 
+			var value 	= localStorage.getItem(key);
+			//convert string from local storage value back to an object by using JSON.parse
+			var obj 	= JSON.parse(value);   
+			var makeSubList	= document.createElement("ul"); 
+			makeli.appendChild(makeSubList); 
+			for (var n in obj){
+				var makeSubli = document.createElement("li");
+				makeSubList.appendChild(makeSubli); 
+				//the [n] is the var/text group within the array of data
+				//[0] is the array's 1st set which is the name
+				//[1] is the array's 2nd set which is the data information for the book
+				var optSubText = obj[n][0]+" "+obj[n][1]; 
+				makeSubli.innerHTML = optSubText; 
+			}
+		}
+	};
 	
 	//Variable defaults
 	var booksGenres 	= ["--Choose A Genre--", "autobiography", "biography", "business_finance", "classics", "comic_graphic", "computers", "cooking", "craft_hobby", "crime", "design", "exercise", "fantasy", "gardening_farming", "health_mind_body", "history", "horror", "house_and_garden", "languages", "music", "outdoor_activities", "paranormal", "poetry", "religion", "romance", "sports", "scifi", "technical", "travel_guides", "true_crime", "witches_wizards_magic", "woodworking"];
@@ -114,9 +149,9 @@ window.addEventListener("DOMContentLoaded", function(){
 	//Set link & Submit Click Events
 	var saveBook 		= $("submitButton"); 
 	saveBook.addEventListener("click", submitInfo);
-/*	var showBook 		= $("showSatchel"); 
+	var showBook 		= $("showSatchel"); 
 	showBook.addEventListener("click", showInfo);
-	var goToForm 		= $("goHome");
+/*	var goToForm 		= $("goHome");
 	goToForm.addEventListener("click", resetForm);
 	var emptySatchel 	= $("emptySatchel");
 	emptySatchel.addEventListener("click", clearInfo);
