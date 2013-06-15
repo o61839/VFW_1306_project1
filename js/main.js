f33db6de7f11385e172abb39c14fe5bd22ca7bd2
Jenney Grover <jenneygrover@fullsail.edu>
2013-06-15 13:13:47 -0400

    updates
    Updated to start project 3.
    deleted old js code and video 1 & 2 watched and code updated.

--- js/main.js
+++ js/main.js
@@ -1,240 +1,46 @@
 /* 	Jenney Grover
 	VFW 1306
-	Project 2
-	June 13, 2013
+	Project 2 - redo
+	June 15, 2013
 */
 
-//Wait until DOM is ready. 
-window.addEventListener("DOMContentLoaded", function (){
-//will update this as I saw that video late today after I did all this code. 
-	//main part of form
-	var nameBook 	= document.getElementById("bname");
-	var nameAuthor 	= document.getElementById("aname");
-	var isbnNumber 	= document.getElementById("isbn");
-	var readBook 	= document.getElementById("read");
-	var readBookNot = document.getElementById("not_read");
-	var dateAdded 	= document.getElementById("dateAdded");
-	var datePublish = document.getElementById("datePublished");
-	var rating		= document.getElementById("rating"); 
-	var genre		= document.getElementById("genre"); 
-	//list of genres
-	var poetry 		= document.getElementById("poetry");
-	var paranormal 	= document.getElementById("paranormal");
-	var romance 	= document.getElementById("romance");
-	var classics 	= document.getElementById("classics");
-	var comicGraphic = document.getElementById("comic_graphic");
-	var scifi 		= document.getElementById("scifi");
-	var crime 		= document.getElementById("crime");
-	var fantasy 	= document.getElementById("fantasy");
-	var horror 		= document.getElementById("horror");
-	var witchesWizardsMagic = document.getElementById("witches_wizards_magic");
-	var autobiography = document.getElementById("autobiography");
-	var design 		= document.getElementById("design");
-	var houseGarden = document.getElementById("house_and_garden");
-	var music 		= document.getElementById("music");
-	var biography 	= document.getElementById("biography");
-	var businessFinance = document.getElementById("business_finance");
-	var languages 	= document.getElementById("languages");
-	var sports 		= document.getElementById("sports");
-	var recipes 	= document.getElementById("recipes");
-	var travelGuides = document.getElementById("travel_guides");
-	var trueCrime 	= document.getElementById("true_crime");
-	var healthMindBody = document.getElementById("health_mind_body");
-	var history 	= document.getElementById("history");
-	var religion 	= document.getElementById("religion");
-	var computers 	= document.getElementById("computers");
-	var technical 	= document.getElementById("technical");
-	var craftHobby 	= document.getElementById("craft_hobby");
-	var exercise 	= document.getElementById("exercise");
-	//optional part of form
-	var purchased 	= document.getElementById("purchased");
-	var borrowed 	= document.getElementById("borrowed");
-	var paperback 	= document.getElementById("paperback");
-	var hardcover 	= document.getElementById("hardcover");
-	var mobile 		= document.getElementById("mobile");
-	var fiction 	= document.getElementById("fiction");
-	var nonFiction 	= document.getElementById("nonfiction");
-	var comments 	= document.getElementById("comments");
-	//three buttons
-	var submitData 	= document.getElementById("submitButton");
-	var showData 	= document.getElementById("showSatchel");
-	var clearData 	= document.getElementById("emptySatchel");
-	var showGo		= document.getElementById("goHome");
-	//for the form...random number generator
-	var keyValue; 
-	var readingSelection = []; 
-	var permanentSelection = []; 
-	var coverSelection = []; 
-	var typeSelection = []; 
-	//container to hold the form's information (i.e. the Book Info)
-	var myData = {};
-	var bookInfoList;
-	var myNewDiv 	= document.getElementById("bookInfoDisplay"); //parent
+//Wait until the DOM is ready. 
+window.addEventListener("DOMContentLoaded", function(){
 
-	//JavaScript Methods: .getElementsByTagName(); 
-
-	var formAddBook = document.getElementsByTagName("form"); 
-
-	//arrays
-
-	var resultsMyData = myData; 
-
-	//Functions
-	
-	function resetForm(){
-			document.getElementById("myBookQuestions").reset();
-   	};
-
-	var readBookChoices = function(){
-		//read not_read
-		var selected 	= document.getElementById("myBookQuestions").choice;
-		for(var s=0, t=selected.length; s<t; s++){
-			if(selected[s].checked){
-				readingSelection.push = selected[s].value; 
-				return readingSelection;
-			}
-		}
-	};
-	var permanentBookChoices = function(){
-		//permanent or borrowing
-		var selectedP 	= document.getElementById("myBookQuestions").permanent;
-		for(var s=0, t=selectedP.length; s<t; s++){
-			if(selectedP[s].checked){
-				permanentSelection.push = selectedP[s].value; 
-				return permanentSelection;
-			}
-		}
-	};
-
-	var coverBookChoices = function(){
-		//hardcover, paperback, mobile
-		var selectedC 	= document.getElementById("myBookQuestions").cover;
-		for(var s=0, t=selectedC.length; s<t; s++){
-			if(selectedC[s].checked){
-				coverSelection.push = selectedC[s].value; 
-				return coverSelection;
-			}
-		}
-	};
-
-	var typeBookChoices = function(){
-		//fiction, non-fiction
-		var selectedT 	= document.getElementById("myBookQuestions").type;
-		for(var s=0, t=selectedT.length; s<t; s++){
-			if(selectedT[s].checked){
-				typeSelection.push = selectedT[s].value; 
-				return typeSelection;
-			}
-		}
-	};
-	//submitInfo function goes with the submitData button (Put in Satchel)
-	var submitInfo = function(){
-		myData.nameBook 	= document.getElementById("bname").value;
-		myData.nameAuthor 	= document.getElementById("aname").value;
-		myData.isbnNumber 	= document.getElementById("isbn").value;
-		myData.dateAdded 	= document.getElementById("dateAdded").value;
-		myData.datePublish 	= document.getElementById("datePublished").value;
-		myData.comments 	= document.getElementById("comments").value;
-		myData.rating		= document.getElementById("rating").value; 
-		myData.genre		= document.getElementById("genre").value;
-		myData.comments		= document.getElementById("comments").value;  
-		//radio buttons: read/not read; purchased/borrowed; 3 cover types; fiction/nonfiction
-		readBookChoices();
-		permanentBookChoices(); 
-		coverBookChoices(); 
-		typeBookChoices();
-		keyValue = Math.floor(Math.random()*100001);
-		localStorage.setItem(keyValue, JSON.stringify(myData)); 
-		alert("Your book is in your Satchel");
-		resetForm();
-	};
-
-	function toggleControls(n){
-		switch(n){
-			case "on":
-				myBookQuestions.style.display = "none"; 
-				clearData.style.display = "inline";
-				submitData.style.visibility = "hidden";
-				showData.style.display = "none"; 
-				showGo.style.display = "inline";
-				myNewDiv.style.display = "block";
-				break;
-			case "off":
-				myBookQuestions.style.display = "block";
-				clearData.style.display = "inline";
-				submitData.style.visibility = "visible"; 
-				showData.style.display = "inline"; 
-				showGo.style.display = "none"; 
-				myNewDiv.style.display = "none";
-				break;
-			default:
-				return false; 
-		}
+	//getElementByID function. 
+	function $(x){
+		var theElement = document.getElementById(x);
+		return theElement; 
 	}
-
-	//showInfo function goes with the showData button (Show Satchel)
-	//Why oh why???
-	var showInfo = function (){
-		toggleControls("on");
-		if(localStorage.length === 0){
-				bookInfoList = 0
-				alert("Your Satchel is empty.");
+	
+	//Create select field element and populate with the options
+	function genreCategory (){
+		var formTag 	= document.getElementsByTagName("myBookQuestions"), //formTag is an array of all the form tags. 
+			selectLi 	= $("select"), //targets list item with id of select
+			makeSelect	= document.createElement("select"); //creates HTML element select
+			makeSelect.setAttribute("id", "genres"); 
+		for (var i=0, j=booksGenres.length; i<j; i++) {//grab options in the variable array
+			var makeOption = document.createElement("option"); 
+			var optText = booksGenres[i]; 
+			makeOption.setAttribute("value", optText); //not id but value in this tag
+			makeOption.innerHTML = optText; 
+			makeSelect.appendChild(makeOption);
 		}
-		//create new tags for data
-		//grandparent = mainContent (body of html)
-		//resultsMyData = array for data content
-		var myNewList 		= document.createElement("ul");  //child
-		myNewList.setAttribute("id", "bookInfo");
-		myNewDiv.appendChild(myNewList);
-		for(i=0, j=localStorage.length; i<j; i++){
-			var myNewLItag	= document.createElement("li"); //another child
-			//myNewLItag.innerHTML = resultsMyData(i);
-			myNewList.appendChild(myNewLItag); 
-			var key = localStorage.key(i); 
-			var value = localStorage.getItem(key); 
-			//convert the string from local storage value back to an object
-			var object = JSON.parse(value);
-			var makeSubList = document.createElement("ul"); 
-			myNewLItag.appendChild(makeSubList); 
-			for(var n in object){
-				var makeSubli = document.createElement("li");
-				makeSubList.appendChild(makeSubli);
-				var bookInfoList = object[n][0]+" "+ object[n][1]; 
-				makeSubli.innerHTML = bookInfoList;
-			}
-		}	
-	};
+		selectLi.appendChild(makeSelect); 
+	}
+	
+	//Variable defaults
+	var booksGenres 	= ["--Choose A Genre--", "autobiography", "biography", "business_finance", "classics", "comic_graphic", "computers", "cooking", "craft_hobby", "crime", "design", "exercise", "fantasy", "gardening_farming", "health_mind_body", "history", "horror", "house_and_garden", "languages", "music", "outdoor_activities", "paranormal", "poetry", "religion", "romance", "sports", "scifi", "technical", "travel_guides", "true_crime", "witches_wizards_magic", "woodworking"];
+	genreCategory (); 
 
-	//clearInfo function goes with the clearData button (Empty Satchel)
-	var clearInfo = function (){
-		if(localStorage.length === 0){
-			alert("There are no books in your Satchel to remove."); 
-		} else {
-			var questionThem = confirm("Pressing OK will empty your Satchel. Are you sure?");
-			if(questionThem){
-			localStorage.clear()
-			alert("Your Satchel is now empty!");
-			} else {
-			alert("Your books are safe in the Satchel"); 
-			}
-			window.location.reload();
-			return false; 
-		}
-	};
+	//Set link & Submit Click Events
+	/*var saveBook 		= $("submitButton"); 
+	saveBook.addEventListener("click", submitInfo);
+	var showBook 		= $("showSatchel"); 
+	showBook.addEventListener("click", showInfo);
+	var goToForm 		= $("goHome");
+	goToForm.addEventListener("click", formHome);
+	var emptySatchel 	= $("emptySatchel");
+	emptySatchel.addEventListener("click", clearInfo);*/
 
-
-	//returnHome function returns the two buttons and shows the Form and hides the Satchel
-	var returnHome = function (){
-		toggleControls("off");		
-	};
-
-
-	//Methods
-
-	submitData.addEventListener("click", submitInfo);
-	showData.addEventListener("click", showInfo);
-	clearData.addEventListener("click", clearInfo);
-	goHome.addEventListener("click", returnHome);
-
-
-});
+}); 
